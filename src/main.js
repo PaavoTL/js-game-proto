ca89c8e13bf385a54e6564b5d4b2846ee3a0a197
PaavoTL <43340576+PaavoTL@users.noreply.github.com>
2018-11-22 10:50:18 +0200

    umm...

---
+++ src/main.js
@@ -0,0 +1,6 @@
+let canvas = document.createElement("canvas");
+
+canvas.width = 800;
+canvas.height = 600;
+
+document.body.appendChild(canvas);
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,15 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  mode: 'development',
+  entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000
   }
 };
