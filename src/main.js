b9adc65ed986919a13815b65d9a0ae9829f10517
PaavoTL <43340576+PaavoTL@users.noreply.github.com>
2018-11-22 10:53:22 +0200

    basic canvas functionality

--- src/main.js
+++ src/main.js
@@ -4,3 +4,8 @@ canvas.width = 800;
 canvas.height = 600;
 
 document.body.appendChild(canvas);
+
+let ctx = canvas.getContext("2d");
+
+ctx.fillStyle = "#303030";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
