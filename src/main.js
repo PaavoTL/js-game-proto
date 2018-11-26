46ff6053ba037e0ae07d75e1ab98b4ed28c28f15
PaavoTL <43340576+PaavoTL@users.noreply.github.com>
2018-11-26 13:37:56 +0200

    add sprite sheet animation system

--- src/main.js
+++ src/main.js
@@ -2,11 +2,13 @@ import Engine from './engine';
 import GameObject from './gameobject';
 import Renderable from './renderable';
 
+import imgKnight from './images/knight.png'
+
 let engine = new Engine();
 
 let testObj1 = new GameObject(100,100);
 
-testObj1.addChild(new Renderable());
+testObj1.addChild(new Renderable(imgKnight, 0, 5, 5, 1, 5, 4));
 
 engine.addObject(testObj1);
 
--- src/renderable.js
+++ src/renderable.js
@@ -1,18 +1,40 @@
 import noIMG from './images/noimg.png'
 
 export default class Renderable{
-    constructor(image = noIMG, startFrame = 0, frameCount = 0, framesX = 1, framesY = 1, frameSpeed = 0){
+    constructor(image = noIMG, startFrame = 0, frameCount = 0, framesX = 1, framesY = 1, animSpeed = 0 ,size = 1){
         this.img = new Image();
         this.img.src = image;
-
+        
+        this.frame = startFrame;
         this.startFrame = startFrame;
         this.frameCount = frameCount;
         this.framesX = framesX;
         this.framesY = framesY;
-        this.frameSpeed = frameSpeed;
+        this.subWidth = this.img.width / framesX;
+        this.subHeight = this.img.height / framesY;
+        this.animSpeed = animSpeed;
+        this.size = size;
+
+        this.animTime = new Date().getTime();
     }
 
     draw(ctx){
-        ctx.drawImage(this.img, 0, 0);
+        console.log(this.frame)
+
+        let t = new Date().getTime();
+        if (t > this.animTime){
+            this.frame ++;
+            this.animTime = t + 1000 / this.animSpeed;
+        }
+
+        if (this.frame > this.startFrame + this.frameCount -1){
+            this.frame = this.startFrame;
+        }
+
+        let posX = (this.frame % this.framesX) * this.subWidth;
+        let posY = Math.floor(this.frame / this.framesX) * this.subHeight;
+
+        ctx.imageSmoothingEnabled = false;
+        ctx.drawImage(this.img, posX, posY, this.subWidth, this.subHeight, 0, 0, this.subWidth * this.size, this.subHeight * this.size);
     }
 }
